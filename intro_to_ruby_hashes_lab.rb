a31d7643bed0061474187ace21d5af6a18518fb7
Paula Tulis <tulis.paula@gmail.com>
2019-03-26 21:13:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,11 @@
-
-
+def new_hash
+  my_hash{}
 end
+
+def actor 
+  
+end 
+
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
